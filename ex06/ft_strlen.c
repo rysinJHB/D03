e99caa7b6c7dc0c4871e16bf304d24eadd88f517
Ryan Sin <rysin@c5r8s5.wethinkcode.co.za>
2020-07-13 11:44:14 +0200

    Seventh commit

--- ex06/ft_strlen.c
+++ ex06/ft_strlen.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int		ft_strlen(char *str)
 {
@@ -10,4 +11,9 @@ int		ft_strlen(char *str)
 		index++;
 	}
 	return (index);
+}	
+int main (void){
+	printf("%d\n", ft_strlen("Hello World!\n"));
+	return(0);
 }
+	
