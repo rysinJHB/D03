523d90be1d35065055365a25a5278217ea9ea783
Ryan Sin <rysin@c5r8s5.wethinkcode.co.za>
2020-07-13 13:57:52 +0200

    Fourth commit

--- ex03/ft_div_mod.c
+++ ex03/ft_div_mod.c
@@ -1,4 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rysin <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/13 13:44:25 by rysin             #+#    #+#             */
+/*   Updated: 2020/07/13 13:56:55 by rysin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
  #include <unistd.h>
+ #include <stdio.h>
 
 void ft_div_mod(int a, int b, int *div, int *mod)
 {
@@ -7,4 +20,19 @@ void ft_div_mod(int a, int b, int *div, int *mod)
 		*div = a / b;
 		*mod = a % b;
 	}
+}
+
+int main()
+{
+  int a = 41;
+  int b = 11;
+  int *div;
+  int *mod;
+
+  ft_div_mod(a, b, div, mod);
+
+  printf("%d\n", *div);
+  printf("%d\n", *mod);
+
+  return(0);
 }
