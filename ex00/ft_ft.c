4255d7a0d2c6666d58c79a4a6cc3dc995d26c124
Ryan Sin <rysin@c5r8s5.wethinkcode.co.za>
2020-07-14 10:42:03 +0200

    First commit

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -6,12 +6,28 @@
 /*   By: rysin <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 12:26:26 by rysin             #+#    #+#             */
-/*   Updated: 2020/07/13 14:09:45 by rysin            ###   ########.fr       */
+/*   Updated: 2020/07/14 10:40:57 by rysin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_ft(int *nbr){
 	*nbr = 42;
 	}
+void 	ft_putnbr(int nbr)
+{
+}
+int main (void)
+{
+	int a;
+	int *ptr;
+
+	a =42;
+	ptr = &a;
+
+		ft_ft(ptr);
+		ft_putnbr(*ptr);
+		return(0);
+}
